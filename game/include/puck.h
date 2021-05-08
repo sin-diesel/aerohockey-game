4da6c7d3b804c44c88d167fd31430fd212d50619
pr0kuk <shcherbakov.aa@phystech.edu>
2021-05-08 22:48:55 +0300

    Added class Striker:Puck

--- game/include/puck.h
+++ game/include/puck.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "game.h"
-
+#define MAX_POS_X 200
+#define MAX_POS_Y 200
+#define MIN_POS_X 0
+#define MIN_POS_Y 0
+#define RADIUS 10
 class Puck {
-    private:
+    protected:
     sf::Vector2f position;
     sf::Vector2f speed;
     public:
-    sf::Vector2f update();
-    void change_speed(sf::Vector2f pos);
+    sf::Vector2f update(sf::Vector2f, sf::Vector2f);
+    sf::Vector2f get_coord();
+};
+
+class Striker : public Puck {
+    public:
+    void calculate_speed(sf::Vector2f);
 };
