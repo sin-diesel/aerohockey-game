5b2d13eb3147362444234d70392ac107f8f4cc1d
Sergey <Koshelev.SM@phystech.edu>
2021-05-10 19:32:33 +0300

    finish strikers

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -3,8 +3,8 @@
 Game::Game(sf::Vector2u windowsize_) {
     sf::Vector2f windowsize = sf::Vector2f(windowsize_);
     scoreboard = Scoreboard("/home/sergey/aerohockey-game/game/images/scoreboard.png", {windowsize.x/2, 0});  
-    striker1 = DynamicObject("/home/sergey/aerohockey-game/game/images/circle.jpg");
-    striker2 = DynamicObject("/home/sergey/aerohockey-game/game/images/circle.jpg");
+    striker1 = DynamicObject("/home/sergey/aerohockey-game/game/images/striker.png");
+    striker2 = DynamicObject("/home/sergey/aerohockey-game/game/images/striker.png");
     puck = DynamicObject("/home/sergey/aerohockey-game/game/images/puck.png");
    
     puck.set_coord({windowsize.x / 2, windowsize.y / 2});  //temporary, just to look vizualization
@@ -20,7 +20,7 @@ void Game::play(sf::RenderWindow& window) {
     //puck.update({150, 50}, {0, 0});
     //scoreboard.update();
     sf::Vector2f mouse_pos = sf::Vector2f(sf::Mouse::getPosition(window));
-    striker1.set_coord(mouse_pos);
+    striker1.set_coord(mouse_pos); //temporary, just to look vizualization
 }
 
 void Game::draw_objects(sf::RenderWindow& window) {
