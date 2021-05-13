97340156fa9e60a4e75fa83f1481b9c10890da7c
Sergey <Koshelev.SM@phystech.edu>
2021-05-13 15:21:37 +0300

    add menu

--- game/src/main_client.cpp
+++ game/src/main_client.cpp
@@ -1,13 +1,12 @@
 #include "../include/library.h"
 #include "../include/game.h"
 
-int main() {
+void game(sf::RenderWindow& window)
+{
     sf::Clock clock;
     sf::Time elapsed;
     int pause_flag = 0;
 
-    // window
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "aerohockey-game", sf::Style::Default);
     Game aerohockey(window.getSize());
     std::cout << "aerohockey number is " << aerohockey.number << std::endl;
     sf::Image background_image;
@@ -64,6 +63,59 @@ int main() {
             window.display();
         }
     }
+}
 
+bool menu(sf::RenderWindow& window)
+{
+    #ifdef __MACH__
+        std::string path = "/Users/stassidelnikov/aerohockey-game";
+    #else
+        std::string path = std::experimental::filesystem::current_path().string();
+    #endif
+
+    sf::Texture buttonTexture;
+    buttonTexture.loadFromFile(path + "/game/images/button.png");
+    sf::Sprite menubutton(buttonTexture), exitbutton(buttonTexture);
+    bool isMenu = true;
+    int menuNum = 0;
+	menubutton.setPosition(100, 30);
+    exitbutton.setPosition(100, 330);
+
+	while (isMenu)
+	{
+        std::cout << "menu loop" << std::endl;
+		menubutton.setColor(sf::Color::White);
+        exitbutton.setColor(sf::Color::White);
+		menuNum = 0;
+		window.clear(sf::Color(129, 181, 221));
+ 
+		if (sf::IntRect(100, 30, 300, 50).contains(sf::Mouse::getPosition(window))) { menubutton.setColor(sf::Color::Blue); menuNum = 1; }
+        if (sf::IntRect(100, 330, 300, 450).contains(sf::Mouse::getPosition(window))) { exitbutton.setColor(sf::Color::Blue); menuNum = 2; }
+ 
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+            std::cout << "button pressed " << menuNum << std::endl;
+			if (menuNum == 1) { 
+                isMenu = false;//если нажали первую кнопку, то game
+                game(window);
+                return true;
+            } 
+			if (menuNum == 2)  { 
+                isMenu = false; 
+            }
+ 
+		}
+ 
+		window.draw(menubutton);
+		window.draw(exitbutton);
+		window.display();
+	}
+    return true;
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "aerohockey-game", sf::Style::Default);
+    menu(window);
+    window.close();
     return 0;
 }
